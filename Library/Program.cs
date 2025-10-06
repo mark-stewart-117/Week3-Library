d7a16bf91934269fa1939827c341d574dc1adcbb
mark-stewart-117 <stewart-m22@ulster.ac.uk>
2025-10-06 15:14:45 +0100

    Fixed typo

--- Library/Program.cs
+++ Library/Program.cs
@@ -6,9 +6,9 @@
 
     void DisplayInfo()
     {
-        Console.WriteLine("$Book title: {Title}");
-        Console.WriteLine("$Book Author: {Author}");
-        Console.WriteLine("$Book ISBN: {ISBN}");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
         Console.WriteLine();
     }
     static void Main(string[] args)
