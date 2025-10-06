46393834af33269f659c55503c902b92e6d74d9d
mark-stewart-117 <stewart-m22@ulster.ac.uk>
2025-10-06 14:47:04 +0100

    Initial Comment

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine("$Book title: {Title}");
+        Console.WriteLine("$Book Author: {Author}");
+        Console.WriteLine("$Book ISBN: {ISBN}");
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        // Create a new instance (object) of the book class
+        //Note how the object name differs from the class name
+        Book book = new Book();
+
+        //This information is for one book in our library
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        //Another book object.
+        //Note the object name is 'book1' and not 'book'
+        Book book1 = new Book();
+        book1.Title = "C# Methods and classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+       //Output book info to the console using DisplayInfo
+
+        book.DisplayInfo();
+        book1.DisplayInfo();
+
+    }
+}
+
